8458f0938d769294fc3a6725a4963a86017ed0f4
ritikadhawan <ritikadhawan28@gmail.com>
2020-04-21 17:33:45 +0530

    update cart route

--- routes/api/cart.js
+++ routes/api/cart.js
@@ -1,7 +1,8 @@
-const Cart = require('../../models/cart');
+const Cart = require("../../models/cart");
+const {Product} = require("../../models/product");
 const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 const errorHandler = require("../../middleware/error");
 
 //route: /api/cart POST
@@ -21,12 +22,37 @@ router.post("/",passport.authenticate('jwt',{session: false}), async(req, res)=>
 
 });
 
-//route: /api/cart GET
-// router.get()
+//route: /api/cart/display POST
+router.post("/display", async (req, res)=>{
+    try{
+        let products = await Cart.findById(req.body.cart).populate({
+            path: 'products'
+        });
+
+        return res.status(200).json({
+            message: "products",
+            products
+        });
+    } catch (err) {
+        errorHandler(err);
+    }
+})
+
+//route: /api/cart/?product_id=123&cart_id=231  DELETE
+//to remove all instances of that product
+router.delete("/", async (req, res)=>{
+    try{
+        let cart = await Cart.findByIdAndUpdate(req.query.cart_id, {
+            $pull: {products: req.query.product_id}
+        });
+        let product = await Product.findById(req.query.product_id);
+        return res.status(200).json({
+            message: "Product deleted",
+            product
+        });
+
+    } catch(err) {
+        errorHandler(err);
+    }
+})
 module.exports = router;
-
-
-// {
-// 	"cart": "5e9ecbc8315fa53bd4fe7486",
-// 	"product": "5e9ecafb315fa53bd4fe7483"
-// }

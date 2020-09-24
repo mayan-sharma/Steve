c0ecbfd2bd7d6984dc4c940e469ab4f3a80bc0da
ritikadhawan <ritikadhawan28@gmail.com>
2020-09-24 20:22:38 +0530

    ..

--- index.js
+++ index.js
@@ -16,13 +16,6 @@ app.use(bodyParser.json());
 //use validator
 // app.use(expressValidator);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 //use routes
 app.use("/api", require("./routes/api"));
 
